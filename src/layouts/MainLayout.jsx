8a7d255409dfa0156dbd36193516f96c08d32c82
Eder Lima <devilsouls666@gmail.com>
2022-01-22 13:18:22 -0300

    fix: add default width to main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,9 +4,9 @@ import Header from '@/components/ui/Header'
 
 export default function MainLayout({ children }) {
   return (
-    <main className="bg-black">
+    <main className="bg-black w-full min-h-screen">
       <Header />
-      <div className="mx-auto max-w-screen-xl">{children}</div>
+      <div className="mx-auto w-[90%] max-w-screen-xl">{children}</div>
     </main>
   )
 }
